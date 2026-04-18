fb253e9aef5053095c3c04a7c581aa7917de56d7
Seth <47252832+Sethy1124@users.noreply.github.com>
2026-04-18 18:38:13 -0500

    Update sar_start.bat to modify server execution

--- sar_start.bat
+++ sar_start.bat
@@ -1,11 +1,5 @@
 @echo off
 set TXHOST_FXS_PORT={{ServerPort}}
 set TXHOST_TXA_PORT={{txPort}}
-FXServer.exe
+server/FXServer.exe
 pause
-
-{{FullBaseDir}}
-{{$FullBaseDir}}
-
-{{FullInstanceDir}}
-{{$FullInstanceDir}}
