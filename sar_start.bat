c051881bc979ade9dcfc8756d2e73dfda3fc12d9
Seth <47252832+Sethy1124@users.noreply.github.com>
2026-04-18 22:05:47 -0500

    Fix variable syntax in sar_start.bat

--- sar_start.bat
+++ sar_start.bat
@@ -1,6 +1,6 @@
 @echo off
 set TXHOST_FXS_PORT={{ServerPort}}
 set TXHOST_TXA_PORT={{txPort}}
-set rcon_password {{$RemoteAdminPassword}}
+set rcon_password {{RemoteAdminPassword}}
 server\FXServer.exe
 pause
